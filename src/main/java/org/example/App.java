86ea47c5da64f724252155a43618f9350a467c3a
Davide-Ettori <ettodavi01@gmail.com>
2023-02-27 21:11:10 +0100

    primo commit per connettere github ad intellij

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App{
+    public static void main( String[] args ){
+        System.out.println( "\nHello World!" );
+
+        int a = 10, b = 20;
+        int c = a + b;
+    }
+}
