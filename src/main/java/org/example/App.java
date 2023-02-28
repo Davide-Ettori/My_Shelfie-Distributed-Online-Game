2a80ba4a0f40e73581397938144d8759237c1f72
Gioforno <samuelefac@gmail.com>
2023-02-28 16:29:10 +0100

    Revert "a14 b8 one class"
    This reverts commit 505b6214

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,7 +9,7 @@ public class App{
     public static void main( String[] args ){
         System.out.println( "\nHello World!" );
 
-        int a = 12, b = 5;
+        int a = 13, b = 6;
         int c = a + b;
         int modifica = 100;
     }
@@ -18,8 +18,7 @@ class App1{
     public static void main( String[] args ){
         System.out.println( "\nHello World!" );
 
-        int a = 12, b = 5;
-        int c = a + b;
+
         int pluto = 100;
     }
 }
