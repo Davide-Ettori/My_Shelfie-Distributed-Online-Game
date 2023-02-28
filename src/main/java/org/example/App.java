adfaed8c3c78e99d9d738ed5c3194b4c36b50d83
Gioforno <samuelefac@gmail.com>
2023-02-28 16:21:48 +0100

    Revert "a12"
    This reverts commit 7524a77a

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,7 +9,7 @@ public class App{
     public static void main( String[] args ){
         System.out.println( "\nHello World!" );
 
-        int a = 12, b = 7;
+        int a = 700, b = 5;
         int c = a + b;
         int modifica = 100;
     }
@@ -18,9 +18,7 @@ class App1{
     public static void main( String[] args ){
         System.out.println( "\nHello World!" );
 
-
-
-        int a = 12, b = 7;
+        int a = 12, b = 5;
         int c = a + b;
         int pluto = 100;
     }
