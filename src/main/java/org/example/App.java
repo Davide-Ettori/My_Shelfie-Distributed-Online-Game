632c2968efe06b1ce97ecb419446906b55d626a6
Gioforno <samuelefac@gmail.com>
2023-02-28 16:29:42 +0100

    a14 b7 one class

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,7 +9,7 @@ public class App{
     public static void main( String[] args ){
         System.out.println( "\nHello World!" );
 
-        int a = 13, b = 6;
+        int a = 14, b = 7;
         int c = a + b;
         int modifica = 100;
     }
