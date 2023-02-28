2bb22060c35e14755a752e6195f79bc0376e5e73
Gioforno <samuelefac@gmail.com>
2023-02-28 16:15:04 +0100

    a700

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,7 +9,7 @@ public class App{
     public static void main( String[] args ){
         System.out.println( "\nHello World!" );
 
-        int a = 299, b = 5;
+        int a = 700, b = 5;
         int c = a + b;
         int modifica = 100;
     }
