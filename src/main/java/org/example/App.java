d06d35c5229738ecfd151b5dd1c5812a49162f94
Gioforno <samuelefac@gmail.com>
2023-02-28 16:20:11 +0100

    Revert "pippo inserita"
    This reverts commit f9569b3f

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,19 +9,7 @@ public class App{
     public static void main( String[] args ){
         System.out.println( "\nHello World!" );
 
-        int a = 12, b = 7;
+        int a = 12, b = 5;
         int c = a + b;
-        int modifica = 100;
-    }
-}
-class App1{
-    public static void main( String[] args ){
-        System.out.println( "\nHello World!" );
-
-
-
-        int a = 12, b = 7;
-        int c = a + b;
-        int pluto = 100;
     }
 }
