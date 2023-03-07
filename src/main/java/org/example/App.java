d55b22f81bd978e9ccdfbe172692d5a88daefc5a
Davide-Ettori <ettodavi01@gmail.com>
2023-03-07 13:13:52 +0100

    terminale

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,7 +9,7 @@ public class App{
     public static void main( String[] args ){
         System.out.println( "\nHello World!" );
 
-        int a = 14, b = 25;
+        int a = 14, b = 27;
         int c = a + b;
         int modifica = 12;
     }
