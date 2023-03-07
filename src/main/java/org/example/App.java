8da0bc0e04863924e3eb425469e75dd6ecf4dfc1
Davide-Ettori <ettodavi01@gmail.com>
2023-03-07 21:05:14 +0100

    b = 60

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,7 +9,7 @@ public class App{
     public static void main( String[] args ){
         System.out.println( "\nHello World!" );
 
-        int a = 14, b = 49;
+        int a = 14, b = 60;
         int c = a + b;
         int modifica = 12;
     }
