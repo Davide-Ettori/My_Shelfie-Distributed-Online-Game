606ebfaa9fe61046f61ee52b88f9efb889db2b7f
Davide-Ettori <ettodavi01@gmail.com>
2023-03-09 15:42:44 +0100

    file aggiornati

--- src/main/java/playground/rmi/Server.java
+++ src/main/java/playground/rmi/Server.java
@@ -5,7 +5,7 @@ import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 public class Server {
-    public static final int PORT = 5000; // porta di default per RMI
+    public static final int PORT = 3000; // porta di default per RMI
     public static void greet(Greetings server, String msg) throws Exception {
         server.getClient().sendMsg("Ciao " + msg, true);
     }
@@ -24,20 +24,3 @@ public class Server {
         System.out.println("fine");
     }
 }
-
-/*
-
-System.setProperty("java.rmi.server.hostname", "127.0.0.1"); // fisso hostname su localhost
-        try{
-        System.out.println("inizio");
-        PacketImpl pac = new PacketImpl("Hello World"); // creo il pacchetto
-        Packet stubPac = (Packet) UnicastRemoteObject.exportObject(pac, 0); // esporto l'oggetto che ho creato poco fa
-
-        Registry registry = LocateRegistry.getRegistry("127.0.0.1", PORT); // apro la connessione sulla porta 5000
-        registry.rebind("pacFromServer", stubPac); // ora il client vedrà l'oggetto stubPac con il nome pacFromServer
-
-        }catch(Exception e){
-        System.out.println("\nErrore sul server: " + e.toString());
-        }
-
- */
