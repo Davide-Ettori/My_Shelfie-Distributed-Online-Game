c257afd323591d7d71896c7751025234b15dc1d3
Davide-Ettori <ettodavi01@gmail.com>
2023-03-09 14:46:12 +0100

    aggiunti file

---
+++ src/main/java/playground/GUI/java_fx/ExampleGUI.java
@@ -0,0 +1,7 @@
+package playground.GUI.java_fx;
+
+public class ExampleGUI {
+    public static void main(String[] args){
+        System.out.println("\nHello World");
+    }
+}
--- src/main/java/playground/rmi/Greetings.java
+++ src/main/java/playground/rmi/Greetings.java
@@ -4,23 +4,34 @@ import java.rmi.Remote;
 import java.rmi.server.UnicastRemoteObject;
 
 interface GreetingsInterface extends Remote{ // interfaccia, non classe
-    //public String getMsg() throws Exception; // devi sempre aggiungere questa eccezione
-    public void getMsg() throws Exception;
+    public String getMsg() throws Exception; // devi sempre aggiungere questa eccezione
+    public void setMsg(String msg) throws Exception;
+    public void setClient(GreetingsInterface g) throws Exception;
+    public GreetingsInterface getClient() throws Exception;
 }
 
 public class Greetings extends UnicastRemoteObject implements GreetingsInterface {
     private String msg;
+    private GreetingsInterface client;
 
     public Greetings(String msg) throws Exception{ // anche qui è obbligatoria l'eccezione
         this.msg = msg;
     }
 
-    public void getMsg() throws Exception{
-        System.out.println(this.msg);
-        //return this.msg;
+    public String getMsg() throws Exception{
+        //System.out.println(this.msg);
+        return this.msg;
     }
 
     public void setMsg(String msg){
         this.msg = msg;
+    }
+
+    public void setClient(GreetingsInterface g){
+        this.client = g;
+    }
+
+    public GreetingsInterface getClient(){
+        return this.client;
     }
 }
