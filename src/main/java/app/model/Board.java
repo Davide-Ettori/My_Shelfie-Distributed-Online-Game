c78ff200697db37547198d38ddd67aac363f3e35
Davide-Ettori <ettodavi01@gmail.com>
2023-03-21 21:31:51 +0100

    add ClientBoard methods

--- src/main/java/app/model/Board.java
+++ src/main/java/app/model/Board.java
@@ -6,4 +6,7 @@ interface Board {
     void updateBoard(Board board);
     void sendCurrentBoard();
     private void Draw() {}
+    Card[][] getGameBoard(); // ho definito alcuni getter che saranno necessari per alcune variabili non costanti
+    CommonObjective getCO_1();
+    CommonObjective getCO_2();
 }
