4c55e6c13e189eb66cd947655e31333f3aa28c5b
Davide-Ettori <ettodavi01@gmail.com>
2023-03-23 15:20:56 +0100

    small fix

--- src/main/java/app/model/Algo_CO_9.java
+++ src/main/java/app/model/Algo_CO_9.java
@@ -1,6 +1,9 @@
 package app.model;
 
 import java.util.ArrayList;
+
+import static app.model.Color.EMPTY;
+
 /*
 Tre colonne formate ciascuna da
 6 tessere di uno, due o tre tipi differenti.
@@ -8,6 +11,13 @@ Colonne diverse possono avere
 combinazioni diverse di tipi di tessere.
  */
 public class Algo_CO_9 extends Strategy { // quinto prima colonna
+    private boolean notEmptyOnCol(Card[][] board, int c){
+        for(int i = 0; i < ROWS; i++){
+            if(board[i][c].color == EMPTY)
+                return false;
+        }
+        return true;
+    }
     @Override
     public boolean checkMatch(Card[][] board) {
         int count = 0;
@@ -18,7 +28,7 @@ public class Algo_CO_9 extends Strategy { // quinto prima colonna
                 if (!colors.contains(board[i][j].color))
                     colors.add(board[i][j].color);
             }
-            if (colors.size() >= 1 && colors.size() <= 3)
+            if (colors.size() >= 1 && colors.size() <= 3 && notEmptyOnCol(board, j))
                 count++;
         }
         return count >= 3;
