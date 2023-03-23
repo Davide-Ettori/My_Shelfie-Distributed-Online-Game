27fc1f19f13789638125094a5e8e3a15420ca781
gfurkan00 <furkan.gumus@mail.polimi.it>
2023-03-23 13:27:05 +0100

    PrivateObjective test completed

--- src/test/java/private_objective/countMatchTest.java
+++ src/test/java/private_objective/countMatchTest.java
@@ -22,6 +22,86 @@ public class countMatchTest {
     public void tearDown() {
         return;
     }
+
+    @Test
+    public void countMatch_personalGoal_0match(){
+        Card[][] objMat = new Card[6][5];
+        objMat[0][0] = new Card(PINK);
+        objMat[0][1] = new Card();
+        objMat[0][2] = new Card(BLUE);
+        objMat[0][3] = new Card();
+        objMat[0][4] = new Card();
+
+        objMat[1][0] = new Card();
+        objMat[1][1] = new Card();
+        objMat[1][2] = new Card();
+        objMat[1][3] = new Card();
+        objMat[1][4] = new Card(GREEN);
+
+        objMat[2][0] = new Card();
+        objMat[2][1] = new Card();
+        objMat[2][2] = new Card();
+        objMat[2][3] = new Card(WHITE);
+        objMat[2][4] = new Card();
+
+        objMat[3][0] = new Card();
+        objMat[3][1] = new Card(YELLOW);
+        objMat[3][2] = new Card();
+        objMat[3][3] = new Card();
+        objMat[3][4] = new Card();
+
+        objMat[4][0] = new Card();
+        objMat[4][1] = new Card();
+        objMat[4][2] = new Card();
+        objMat[4][3] = new Card();
+        objMat[4][4] = new Card();
+
+        objMat[5][0] = new Card();
+        objMat[5][1] = new Card();
+        objMat[5][2] = new Card(CYAN);
+        objMat[5][3] = new Card();
+        objMat[5][4] = new Card();
+
+
+        lib[0][0] = new Card(BLUE);
+        lib[0][1] = new Card();
+        lib[0][2] = new Card();
+        lib[0][3] = new Card();
+        lib[0][4] = new Card(YELLOW);
+
+        lib[1][0] = new Card(BLUE);
+        lib[1][1] = new Card();
+        lib[1][2] = new Card();
+        lib[1][3] = new Card();
+        lib[1][4] = new Card(YELLOW);
+
+        lib[2][0] = new Card(BLUE);
+        lib[2][1] = new Card();
+        lib[2][2] = new Card();
+        lib[2][3] = new Card();
+        lib[2][4] = new Card(YELLOW);
+
+        lib[3][0] = new Card(BLUE);
+        lib[3][1] = new Card();
+        lib[3][2] = new Card();
+        lib[3][3] = new Card();
+        lib[3][4] = new Card(YELLOW);
+
+        lib[4][0] = new Card(BLUE);
+        lib[4][1] = new Card();
+        lib[4][2] = new Card();
+        lib[4][3] = new Card();
+        lib[4][4] = new Card(YELLOW);
+
+        lib[5][0] = new Card(BLUE);
+        lib[5][1] = new Card();
+        lib[5][2] = new Card();
+        lib[5][3] = new Card();
+        lib[5][4] = new Card(YELLOW);
+
+        privateObjective.setObjMatrix(objMat);
+        assertEquals(privateObjective.countPoints(lib), 0);
+    }
     @Test
     public void countMatch_personalGoal_2match(){
         Card[][] objMat = new Card[6][5];
@@ -103,6 +183,161 @@ public class countMatchTest {
     }
     @Test
     public void countMatch_personalgoal_5match(){
+        Card[][] objMat = new Card[6][5];
+        objMat[0][0] = new Card(PINK);
+        objMat[0][1] = new Card();
+        objMat[0][2] = new Card(BLUE);
+        objMat[0][3] = new Card();
+        objMat[0][4] = new Card();
 
+        objMat[1][0] = new Card();
+        objMat[1][1] = new Card();
+        objMat[1][2] = new Card();
+        objMat[1][3] = new Card();
+        objMat[1][4] = new Card(GREEN);
+
+        objMat[2][0] = new Card();
+        objMat[2][1] = new Card();
+        objMat[2][2] = new Card();
+        objMat[2][3] = new Card(WHITE);
+        objMat[2][4] = new Card();
+
+        objMat[3][0] = new Card();
+        objMat[3][1] = new Card(YELLOW);
+        objMat[3][2] = new Card();
+        objMat[3][3] = new Card();
+        objMat[3][4] = new Card();
+
+        objMat[4][0] = new Card();
+        objMat[4][1] = new Card();
+        objMat[4][2] = new Card();
+        objMat[4][3] = new Card();
+        objMat[4][4] = new Card();
+
+        objMat[5][0] = new Card();
+        objMat[5][1] = new Card();
+        objMat[5][2] = new Card(CYAN);
+        objMat[5][3] = new Card();
+        objMat[5][4] = new Card();
+
+
+        lib[0][0] = new Card(PINK);
+        lib[0][1] = new Card(YELLOW);
+        lib[0][2] = new Card(BLUE);
+        lib[0][3] = new Card(YELLOW);
+        lib[0][4] = new Card(YELLOW);
+
+        lib[1][0] = new Card(BLUE);
+        lib[1][1] = new Card(YELLOW);
+        lib[1][2] = new Card(GREEN);
+        lib[1][3] = new Card(GREEN);
+        lib[1][4] = new Card(GREEN);
+
+        lib[2][0] = new Card(BLUE);
+        lib[2][1] = new Card(YELLOW);
+        lib[2][2] = new Card(GREEN);
+        lib[2][3] = new Card(WHITE);
+        lib[2][4] = new Card(WHITE);
+
+        lib[3][0] = new Card(BLUE);
+        lib[3][1] = new Card(BLUE);
+        lib[3][2] = new Card(GREEN);
+        lib[3][3] = new Card(PINK);
+        lib[3][4] = new Card(PINK);
+
+        lib[4][0] = new Card(BLUE);
+        lib[4][1] = new Card(YELLOW);
+        lib[4][2] = new Card(WHITE);
+        lib[4][3] = new Card(CYAN);
+        lib[4][4] = new Card(CYAN);
+
+        lib[5][0] = new Card(BLUE);
+        lib[5][1] = new Card(YELLOW);
+        lib[5][2] = new Card(CYAN);
+        lib[5][3] = new Card(CYAN);
+        lib[5][4] = new Card(PINK);
+
+        privateObjective.setObjMatrix(objMat);
+        assertEquals(privateObjective.countPoints(lib), 9);
+    }
+
+    @Test
+    public void countMatch_personalgoal_6match(){
+        Card[][] objMat = new Card[6][5];
+        objMat[0][0] = new Card(PINK);
+        objMat[0][1] = new Card();
+        objMat[0][2] = new Card(BLUE);
+        objMat[0][3] = new Card();
+        objMat[0][4] = new Card();
+
+        objMat[1][0] = new Card();
+        objMat[1][1] = new Card();
+        objMat[1][2] = new Card();
+        objMat[1][3] = new Card();
+        objMat[1][4] = new Card(GREEN);
+
+        objMat[2][0] = new Card();
+        objMat[2][1] = new Card();
+        objMat[2][2] = new Card();
+        objMat[2][3] = new Card(WHITE);
+        objMat[2][4] = new Card();
+
+        objMat[3][0] = new Card();
+        objMat[3][1] = new Card(YELLOW);
+        objMat[3][2] = new Card();
+        objMat[3][3] = new Card();
+        objMat[3][4] = new Card();
+
+        objMat[4][0] = new Card();
+        objMat[4][1] = new Card();
+        objMat[4][2] = new Card();
+        objMat[4][3] = new Card();
+        objMat[4][4] = new Card();
+
+        objMat[5][0] = new Card();
+        objMat[5][1] = new Card();
+        objMat[5][2] = new Card(CYAN);
+        objMat[5][3] = new Card();
+        objMat[5][4] = new Card();
+
+
+        lib[0][0] = new Card(PINK);
+        lib[0][1] = new Card(YELLOW);
+        lib[0][2] = new Card(BLUE);
+        lib[0][3] = new Card(YELLOW);
+        lib[0][4] = new Card();
+
+        lib[1][0] = new Card(BLUE);
+        lib[1][1] = new Card(YELLOW);
+        lib[1][2] = new Card(GREEN);
+        lib[1][3] = new Card(GREEN);
+        lib[1][4] = new Card(GREEN);
+
+        lib[2][0] = new Card(BLUE);
+        lib[2][1] = new Card(YELLOW);
+        lib[2][2] = new Card(GREEN);
+        lib[2][3] = new Card(WHITE);
+        lib[2][4] = new Card(GREEN);
+
+        lib[3][0] = new Card(BLUE);
+        lib[3][1] = new Card(YELLOW);
+        lib[3][2] = new Card(GREEN);
+        lib[3][3] = new Card(PINK);
+        lib[3][4] = new Card(PINK);
+
+        lib[4][0] = new Card(BLUE);
+        lib[4][1] = new Card(YELLOW);
+        lib[4][2] = new Card(WHITE);
+        lib[4][3] = new Card(CYAN);
+        lib[4][4] = new Card(CYAN);
+
+        lib[5][0] = new Card(BLUE);
+        lib[5][1] = new Card(YELLOW);
+        lib[5][2] = new Card(CYAN);
+        lib[5][3] = new Card(CYAN);
+        lib[5][4] = new Card(PINK);
+
+        privateObjective.setObjMatrix(objMat);
+        assertEquals(privateObjective.countPoints(lib), 12);
     }
 }
