fc319b59894e26a0bc0c6b84b54b7670e944e4e4
Samuele Giammusso <samuele.giammusso@mail.polimi.it>
2023-03-23 15:28:36 +0100

    Added test for Library Class

--- src/test/java/library/checkColTest.java
+++ src/test/java/library/checkColTest.java
@@ -6,7 +6,9 @@ import org.junit.*;
 import static app.model.Color.*;
 import static org.junit.Assert.assertEquals;
 import app.model.Library;
-public class isFullTest {
+
+public class checkColTest {
+
     Library lib = null;
     @Before // eseguita prima dei test
     public void setUp(){
@@ -18,7 +20,7 @@ public class isFullTest {
     }
 
     @Test // test 1
-    public void isFull_allFull(){
+    public void checkCol_allFull(){
         lib.library[0][0] = new Card(PINK);
         lib.library[0][1] = new Card(PINK);
         lib.library[0][2] = new Card(PINK);
@@ -55,52 +57,11 @@ public class isFullTest {
         lib.library[5][3] = new Card(PINK);
         lib.library[5][4] = new Card(PINK);
 
-        assertEquals(lib.isFull(), true);
+        assertEquals(lib.checkCol(0, 3), false);
     }
 
     @Test // test 2
-    public void isFull_oneEmpty(){
-        lib.library[0][0] = new Card();
-        lib.library[0][1] = new Card(PINK);
-        lib.library[0][2] = new Card(PINK);
-        lib.library[0][3] = new Card(PINK);
-        lib.library[0][4] = new Card(PINK);
-
-        lib.library[1][0] = new Card(PINK);
-        lib.library[1][1] = new Card(BLUE);
-        lib.library[1][2] = new Card(PINK);
-        lib.library[1][3] = new Card(PINK);
-        lib.library[1][4] = new Card(PINK);
-
-        lib.library[2][0] = new Card(BLUE);
-        lib.library[2][1] = new Card(BLUE);
-        lib.library[2][2] = new Card(BLUE);
-        lib.library[2][3] = new Card(BLUE);
-        lib.library[2][4] = new Card(BLUE);
-
-        lib.library[3][0] = new Card(PINK);
-        lib.library[3][1] = new Card(BLUE);
-        lib.library[3][2] = new Card(GREEN);
-        lib.library[3][3] = new Card(PINK);
-        lib.library[3][4] = new Card(PINK);
-
-        lib.library[4][0] = new Card(PINK);
-        lib.library[4][1] = new Card(PINK);
-        lib.library[4][2] = new Card(GREEN);
-        lib.library[4][3] = new Card(PINK);
-        lib.library[4][4] = new Card(PINK);
-
-        lib.library[5][0] = new Card(PINK);
-        lib.library[5][1] = new Card(PINK);
-        lib.library[5][2] = new Card(GREEN);
-        lib.library[5][3] = new Card(PINK);
-        lib.library[5][4] = new Card(PINK);
-
-        assertEquals(lib.isFull(), false);
-    }
-
-    @Test // test 3
-    public void isFull_firstColEmpty(){
+    public void checkCol_emptyColumn_correct(){
         lib.library[0][0] = new Card();
         lib.library[0][1] = new Card(PINK);
         lib.library[0][2] = new Card(PINK);
@@ -137,7 +98,47 @@ public class isFullTest {
         lib.library[5][3] = new Card(PINK);
         lib.library[5][4] = new Card(PINK);
 
-        assertEquals(lib.isFull(), false);
+        assertEquals(lib.checkCol(0, 3), true);
     }
 
+    @Test // test 3
+    public void checkCol_tooManyCards(){
+        lib.library[0][0] = new Card();
+        lib.library[0][1] = new Card(PINK);
+        lib.library[0][2] = new Card(PINK);
+        lib.library[0][3] = new Card(PINK);
+        lib.library[0][4] = new Card(PINK);
+
+        lib.library[1][0] = new Card();
+        lib.library[1][1] = new Card(BLUE);
+        lib.library[1][2] = new Card(PINK);
+        lib.library[1][3] = new Card(PINK);
+        lib.library[1][4] = new Card(PINK);
+
+        lib.library[2][0] = new Card(GREEN);
+        lib.library[2][1] = new Card(BLUE);
+        lib.library[2][2] = new Card(BLUE);
+        lib.library[2][3] = new Card(BLUE);
+        lib.library[2][4] = new Card(BLUE);
+
+        lib.library[3][0] = new Card(GREEN);
+        lib.library[3][1] = new Card(BLUE);
+        lib.library[3][2] = new Card(GREEN);
+        lib.library[3][3] = new Card(PINK);
+        lib.library[3][4] = new Card(PINK);
+
+        lib.library[4][0] = new Card(GREEN);
+        lib.library[4][1] = new Card(PINK);
+        lib.library[4][2] = new Card(GREEN);
+        lib.library[4][3] = new Card(PINK);
+        lib.library[4][4] = new Card(PINK);
+
+        lib.library[5][0] = new Card(GREEN);
+        lib.library[5][1] = new Card(PINK);
+        lib.library[5][2] = new Card(GREEN);
+        lib.library[5][3] = new Card(PINK);
+        lib.library[5][4] = new Card(PINK);
+
+        assertEquals(lib.checkCol(0, 3), false);
+    }
 }
