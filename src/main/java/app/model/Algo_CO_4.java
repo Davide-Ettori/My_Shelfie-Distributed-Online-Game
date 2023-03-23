501537ede01d9d169ac3cc6803dc285d221d40d2
Davide-Ettori <ettodavi01@gmail.com>
2023-03-23 15:14:14 +0100

    small fix

--- src/main/java/app/model/Algo_CO_4.java
+++ src/main/java/app/model/Algo_CO_4.java
@@ -1,6 +1,9 @@
 package app.model;
 
 import java.util.ArrayList;
+
+import static app.model.Color.*;
+
 /*
 Quattro righe formate ciascuna
 da 5 tessere di uno, due o tre tipi
@@ -8,6 +11,13 @@ differenti. Righe diverse possono avere
 combinazioni diverse di tipi di tessere.
  */
 public class Algo_CO_4 extends Strategy { // secondo seconda colonna
+    private boolean notEmptyOnRow(Card[][] board, int r){
+        for(int i = 0; i < COLS; i++){
+            if(board[r][i].color == EMPTY)
+                return false;
+        }
+        return true;
+    }
     @Override
     public boolean checkMatch(Card[][] board) {
         int count = 0;
@@ -18,7 +28,7 @@ public class Algo_CO_4 extends Strategy { // secondo seconda colonna
                 if (!colors.contains(board[i][j].color))
                     colors.add(board[i][j].color);
             }
-            if (colors.size() >= 1 && colors.size() <= 3)
+            if (colors.size() >= 1 && colors.size() <= 3 && notEmptyOnRow(board, i))
                 count++;
         }
         return count >= 4;
