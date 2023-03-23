e43a9c7eea877cf8f4c6ca71a65b316ad045ffd4
Gioforno <samuelefac@gmail.com>
2023-03-23 17:29:50 +0100

    add test for second six algorithm

--- src/test/java/algo/Algo10Test.java
+++ src/test/java/algo/Algo10Test.java
@@ -7,6 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static app.model.Color.*;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class Algo10Test {
@@ -23,8 +24,8 @@ public class Algo10Test {
         return;
     }
 
-    @Test // test 1, problema nel metodo: java.lang.ArrayIndexOutOfBoundsException: Index -1 out of bounds for length 6
-    public void algo10_test1_general() {
+    @Test
+    public void algo10_test1_T() {
         mat[0][0] = new Card(PINK);
         mat[0][1] = new Card(GREEN);
         mat[0][2] = new Card(PINK);
@@ -63,5 +64,44 @@ public class Algo10Test {
 
         assertTrue(algoCo10.checkMatch(mat));
     }
+    @Test
+    public void algo10_test1_F() {
+        mat[0][0] = new Card(PINK);
+        mat[0][1] = new Card(GREEN);
+        mat[0][2] = new Card();
+        mat[0][3] = new Card(GREEN);
+        mat[0][4] = new Card(YELLOW);
 
+        mat[1][0] = new Card(BLUE);
+        mat[1][1] = new Card(PINK);
+        mat[1][2] = new Card(YELLOW);
+        mat[1][3] = new Card(YELLOW);
+        mat[1][4] = new Card();
+
+        mat[2][0] = new Card(PINK);
+        mat[2][1] = new Card(CYAN);
+        mat[2][2] = new Card(PINK);
+        mat[2][3] = new Card(BLUE);
+        mat[2][4] = new Card(BLUE);
+
+        mat[3][0] = new Card();
+        mat[3][1] = new Card(YELLOW);
+        mat[3][2] = new Card(GREEN);
+        mat[3][3] = new Card(PINK);
+        mat[3][4] = new Card(PINK);
+
+        mat[4][0] = new Card(PINK);
+        mat[4][1] = new Card(PINK);
+        mat[4][2] = new Card();
+        mat[4][3] = new Card(CYAN);
+        mat[4][4] = new Card(CYAN);
+
+        mat[5][0] = new Card(PINK);
+        mat[5][1] = new Card(GREEN);
+        mat[5][2] = new Card(GREEN);
+        mat[5][3] = new Card(PINK);
+        mat[5][4] = new Card(PINK);
+
+        assertFalse(algoCo10.checkMatch(mat));
+    }
 }
