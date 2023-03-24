85e6d01539fb3aa9f78c463d5f91894821f41645
Gioforno <samuelefac@gmail.com>
2023-03-24 11:24:49 +0100

    case false for algo2

--- src/main/java/app/Initaliazer.java
+++ src/main/java/app/Initaliazer.java
@@ -2,4 +2,12 @@ package app;
 
 public class Initaliazer { // Inizializza lo stato iniziale (casuale) del gioco
     // solo classi statiche
+
+
+    //bucket of po
+
+
+
+
+    //bucket of co
 }
--- src/test/java/common_objective/Algo2Test.java
+++ src/test/java/common_objective/Algo2Test.java
@@ -6,6 +6,7 @@ import app.model.*;
 import org.junit.*;
 
 import static app.model.Color.*;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 
@@ -187,6 +188,172 @@ public class Algo2Test {
         mat[5][4] = new Card(YELLOW);
 
         assertTrue(algoCo2.checkMatch(mat));
+
+    }
+
+    //false
+    @Test
+    public void algo2_test1_F_bassosx() {
+        mat[0][0] = new Card(GREEN);
+        mat[0][1] = new Card(GREEN);
+        mat[0][2] = new Card(GREEN);
+        mat[0][3] = new Card(GREEN);
+        mat[0][4] = new Card(YELLOW);
+
+        mat[1][0] = new Card(PINK);
+        mat[1][1] = new Card(BLUE);
+        mat[1][2] = new Card(YELLOW);
+        mat[1][3] = new Card();
+        mat[1][4] = new Card(PINK);
+
+        mat[2][0] = new Card(CYAN);
+        mat[2][1] = new Card(CYAN);
+        mat[2][2] = new Card();
+        mat[2][3] = new Card(PINK);
+        mat[2][4] = new Card();
+
+        mat[3][0] = new Card(YELLOW);
+        mat[3][1] = new Card();
+        mat[3][2] = new Card(PINK);
+        mat[3][3] = new Card();
+        mat[3][4] = new Card(YELLOW);
+
+        mat[4][0] = new Card();
+        mat[4][1] = new Card();
+        mat[4][2] = new Card();
+        mat[4][3] = new Card(CYAN);
+        mat[4][4] = new Card(CYAN);
+
+        mat[5][0] = new Card(PINK);
+        mat[5][1] = new Card();
+        mat[5][2] = new Card(GREEN);
+        mat[5][3] = new Card(PINK);
+        mat[5][4] = new Card(PINK);
+
+        assertFalse(algoCo2.checkMatch(mat));
+
+    }
+    @Test
+    public void algo2_test1_F_bassodx() {
+        mat[0][0] = new Card(PINK);
+        mat[0][1] = new Card(PINK);
+        mat[0][2] = new Card(GREEN);
+        mat[0][3] = new Card(GREEN);
+        mat[0][4] = new Card(PINK);
+
+        mat[1][0] = new Card(YELLOW);
+        mat[1][1] = new Card();
+        mat[1][2] = new Card(GREEN);
+        mat[1][3] = new Card(GREEN);
+        mat[1][4] = new Card(PINK);
+
+        mat[2][0] = new Card();
+        mat[2][1] = new Card(YELLOW);
+        mat[2][2] = new Card();
+        mat[2][3] = new Card(BLUE);
+        mat[2][4] = new Card(BLUE);
+
+        mat[3][0] = new Card(PINK);
+        mat[3][1] = new Card();
+        mat[3][2] = new Card();
+        mat[3][3] = new Card();
+        mat[3][4] = new Card(PINK);
+
+        mat[4][0] = new Card(PINK);
+        mat[4][1] = new Card(PINK);
+        mat[4][2] = new Card();
+        mat[4][3] = new Card(YELLOW);
+        mat[4][4] = new Card();
+
+        mat[5][0] = new Card(PINK);
+        mat[5][1] = new Card(GREEN);
+        mat[5][2] = new Card(GREEN);
+        mat[5][3] = new Card();
+        mat[5][4] = new Card(YELLOW);
+
+        assertFalse(algoCo2.checkMatch(mat));
+
+    }
+    @Test
+    public void algo2_test1_F_unodx() {
+        mat[0][0] = new Card(PINK);
+        mat[0][1] = new Card(PINK);
+        mat[0][2] = new Card(GREEN);
+        mat[0][3] = new Card();
+        mat[0][4] = new Card(YELLOW);
+
+        mat[1][0] = new Card(GREEN);
+        mat[1][1] = new Card(PINK);
+        mat[1][2] = new Card();
+        mat[1][3] = new Card(YELLOW);
+        mat[1][4] = new Card();
+
+        mat[2][0] = new Card(CYAN);
+        mat[2][1] = new Card();
+        mat[2][2] = new Card(YELLOW);
+        mat[2][3] = new Card();
+        mat[2][4] = new Card(BLUE);
+
+        mat[3][0] = new Card();
+        mat[3][1] = new Card();
+        mat[3][2] = new Card();
+        mat[3][3] = new Card(PINK);
+        mat[3][4] = new Card(PINK);
+
+        mat[4][0] = new Card(YELLOW);
+        mat[4][1] = new Card();
+        mat[4][2] = new Card(GREEN);
+        mat[4][3] = new Card(GREEN);
+        mat[4][4] = new Card(PINK);
+
+        mat[5][0] = new Card(GREEN);
+        mat[5][1] = new Card(GREEN);
+        mat[5][2] = new Card(GREEN);
+        mat[5][3] = new Card(PINK);
+        mat[5][4] = new Card(PINK);
+
+        assertFalse(algoCo2.checkMatch(mat));
+
+    }
+    @Test
+    public void algo2_test1_F_unosx() {
+        mat[0][0] = new Card(PINK);
+        mat[0][1] = new Card();
+        mat[0][2] = new Card(GREEN);
+        mat[0][3] = new Card(GREEN);
+        mat[0][4] = new Card(YELLOW);
+
+        mat[1][0] = new Card();
+        mat[1][1] = new Card(PINK);
+        mat[1][2] = new Card();
+        mat[1][3] = new Card(YELLOW);
+        mat[1][4] = new Card(YELLOW);
+
+        mat[2][0] = new Card(CYAN);
+        mat[2][1] = new Card();
+        mat[2][2] = new Card(PINK);
+        mat[2][3] = new Card();
+        mat[2][4] = new Card(BLUE);
+
+        mat[3][0] = new Card(GREEN);
+        mat[3][1] = new Card(YELLOW);
+        mat[3][2] = new Card();
+        mat[3][3] = new Card();
+        mat[3][4] = new Card();
+
+        mat[4][0] = new Card(YELLOW);
+        mat[4][1] = new Card(YELLOW);
+        mat[4][2] = new Card(YELLOW);
+        mat[4][3] = new Card();
+        mat[4][4] = new Card(PINK);
+
+        mat[5][0] = new Card(YELLOW);
+        mat[5][1] = new Card(GREEN);
+        mat[5][2] = new Card(GREEN);
+        mat[5][3] = new Card(YELLOW);
+        mat[5][4] = new Card(YELLOW);
+
+        assertFalse(algoCo2.checkMatch(mat));
 
     }
 }
