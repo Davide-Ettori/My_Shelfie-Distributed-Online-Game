49a22c238ca51cce7bf9327f9a2f3a20c3d09516
Davide-Ettori <ettodavi01@gmail.com>
2023-03-26 16:23:07 +0200

    small fix

--- src/main/java/playground/GUI/swing/GUI.java
+++ src/main/java/playground/GUI/swing/GUI.java
@@ -9,6 +9,7 @@ public class GUI{
     private JFrame frame;
     private JLabel label;
     private JPanel panel;
+    private JTextField text;
     private final int maxPoints = 8;
 
     public static void main(String[] args) {
@@ -39,11 +40,23 @@ public class GUI{
             label.setText("Numero di punti: " + ++count);
         });
 
+        JButton buttonPrint = new JButton("Stampa");
+        buttonPrint.addActionListener((e) ->{
+            showMessageDialog(null, "\nBravo " + text.getText() + ", hai fatto " + count + " punti!");
+        });
+
         label = new JLabel("Numero di punti: " + count);
+
+        text = new JTextField(20);
+        text .setBounds(100, 20, 165, 25);
+        text.setText("Inserisci il tuo nome");
 
         panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(50,50,50,50));
         panel.setLayout(new GridLayout(0,3));
+        panel.add(text);
+        panel.add(new JPanel());
+        panel.add(buttonPrint);
         panel.add(buttonUp);
         panel.add(new JPanel());
         panel.add(buttonDown);
