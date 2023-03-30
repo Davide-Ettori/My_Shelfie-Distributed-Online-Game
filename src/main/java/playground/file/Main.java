803d91532fdc19d97e3a835b002a3a4ee7268277
Davide-Ettori <ettodavi01@gmail.com>
2023-03-30 22:33:50 +0200

    small fix

--- src/main/java/playground/file/Main.java
+++ src/main/java/playground/file/Main.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
-
+// esempio chiaro di come scrivere e leggere un file in Java
 public class Main {
     public static void main(String[] args){
         try {
@@ -12,7 +12,7 @@ public class Main {
             bw.write("ciao");
             bw.close();
             BufferedReader br = new BufferedReader(new FileReader("src/main/java/playground/file/file.txt"));
-            System.out.println(br.readLine());
+            System.out.println("\n" + br.readLine());
         }
         catch(Exception e){System.out.println(e);}
     }
