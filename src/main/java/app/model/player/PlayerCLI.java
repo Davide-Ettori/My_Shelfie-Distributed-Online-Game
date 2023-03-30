45452cc3fb2c63d39239c52d547c6791d16a7203
gfurkan00 <furkan.gumus@mail.polimi.it>
2023-03-30 14:25:47 +0200

    small fix

--- src/main/java/app/model/player/PlayerCLI.java
+++ src/main/java/app/model/player/PlayerCLI.java
@@ -272,15 +272,15 @@ public class PlayerCLI implements Serializable{
     public void drawAll(){
         /*System.out.print("\033[H\033[2J");
         System.out.flush();*/
-        for(int i=0;i<12;i++){
-            System.out.println();
-        }
         System.out.println(activeName+"'s round");
         board.commonObjective_1.draw();
         board.commonObjective_1.draw();
         objective.draw();
         board.draw();
         printLibrary();
+        for(int i=0;i<12;i++){
+            System.out.println();
+        }
     }
     /** ------------------------------------------------------------------------------------------------------------- */
     public void clone(PlayerCLI p){ // copia la versione sul server dentro a quella del client
