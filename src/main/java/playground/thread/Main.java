51cfc7087f1ad9e69198956ae85155ec2c472667
Davide-Ettori <ettodavi01@gmail.com>
2023-04-04 09:15:26 +0200

    small fix

---
+++ src/main/java/playground/thread/Main.java
@@ -0,0 +1,41 @@
+package playground.thread;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        Thread t = new ProvaThread("davide");
+        t.start();
+        Thread.sleep(5000);
+        t.interrupt();
+    }
+}
+
+class ProvaThread extends Thread{
+    String name;
+    public ProvaThread(String n){
+        name = n;
+    }
+    @Override
+    public void run(){
+        try {
+            Scanner in = new Scanner(System.in);
+            String s;
+            while (true) {
+                System.out.println("Inserisci input (" + name + "): ");
+                while (System.in.available() == 0) {
+                    //System.out.println("check");
+                    Thread.sleep(100);
+                    if (isInterrupted()) {
+                        System.out.println("exit");
+                        return;
+                    }
+                }
+                s = in.nextLine();
+                System.out.println(s);
+            }
+        }
+        catch (InterruptedException e){System.out.println("exit with success");}
+        catch(Exception e){System.out.println(e);}
+    }
+}
