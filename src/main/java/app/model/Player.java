1a0c95ca2900810ab8a1efa4eb512b0f70364b42
Davide-Ettori <ettodavi01@gmail.com>
2023-04-06 19:18:28 +0200

    small fix

--- src/main/java/app/model/Player.java
+++ src/main/java/app/model/Player.java
@@ -77,7 +77,7 @@ public class Player implements Serializable{
         netMode = mode;
         System.out.println("\nSoon you will need to enter your nickname for the game");
         try {
-            mySocket = new Socket(DAVIDE_XIAOMI_IP_G, Server.PORT);
+            mySocket = new Socket(LOCAL_HOST, Server.PORT);
             outStream = new ObjectOutputStream(mySocket.getOutputStream());
             inStream = new ObjectInputStream(mySocket.getInputStream());
         }catch (Exception e){System.out.println("\nServer is either full or inactive, try later"); return;}
