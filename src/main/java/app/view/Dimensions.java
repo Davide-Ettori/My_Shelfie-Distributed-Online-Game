fd737a42da3d18855babb50caf5a98825f6f659a
Davide-Ettori <ettodavi01@gmail.com>
2023-04-15 19:56:34 +0200

    small fix

--- src/main/java/app/view/Dimensions.java
+++ src/main/java/app/view/Dimensions.java
@@ -12,7 +12,7 @@ public class Dimensions {
     public static final int boardDim = 400; // every card is 400 / 9 = 44.44, approximately 44 pixels
     public static final int cardDimBoard = 400 / 10 - 2;
     public static final int cardDimLibPrimary = cardDimBoard;
-    public static final int cardDimLibSeconday = cardDimBoard * (libSecondaryDim/5) / (libPrimaryDim/5); //should be circa 35 pixels
+    public static final int cardDimLibSecondary = cardDimBoard * (libSecondaryDim / 5) / (libPrimaryDim / 5); //should be circa 35 pixels
     public static final int textCols = 30;
     public static final int btnW = 200;
     public static final int btnH = 50;
