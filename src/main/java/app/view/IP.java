5b09095a00293ef349b0766a5af41cc2272a83e2
Davide-Ettori <ettodavi01@gmail.com>
2023-04-17 18:45:19 +0200

    small fix

--- src/main/java/app/view/IP.java
+++ src/main/java/app/view/IP.java
@@ -9,7 +9,7 @@ public class IP {
     public static final String DAVIDE_IP_MANTOVA = "192.168.1.21";
     public static final String SAMUG_IP_MILANO = "192.168.1.3";
     public static final String LOCAL_HOST = "127.0.0.1"; //è il computer stesso
-    public static final String activeIP = LOCAL_HOST;
+    public static final String activeIP = DAVIDE_XIAOMI_IP_G;
 
 }
 
