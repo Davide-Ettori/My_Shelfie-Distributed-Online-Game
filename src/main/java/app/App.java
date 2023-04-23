9b6247c757d27a9ea537f4b77fa0801235e1ca26
Davide-Ettori <ettodavi01@gmail.com>
2023-04-23 11:57:57 +0200

    small fix

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -4,5 +4,6 @@ import app.model.Client;
 
 /**
  * first class called in the game, it starts the main process which will lead to the start of the complete game
+ * @author Ettori
  */
 public class App { public static void main(String[] args){new Client();}}
