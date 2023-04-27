c7937c2731eb0ccc428219f1c685e50912d94535
Davide-Ettori <ettodavi01@gmail.com>
2023-04-27 12:34:45 +0200

    small fix

--- src/main/java/app/view/GUI/PlayerGUI.java
+++ src/main/java/app/view/GUI/PlayerGUI.java
@@ -270,7 +270,7 @@ public class PlayerGUI extends Player implements Serializable, PlayerI{
         mainPanel = new JPanel(new GridBagLayout());
 
         //SFONDO:
-        JLabel generalLabelChooseName = new JLabel(new ImageIcon(new ImageIcon("assets/Publisher material/Display_" + new Random().nextInt(5) + 1 + ".jpg").getImage().getScaledInstance(screenSize.width * 5 / 6, screenSize.height * 9 / 10, Image.SCALE_SMOOTH)));
+        JLabel generalLabelChooseName = new JLabel(new ImageIcon(new ImageIcon("assets/Publisher material/Display_" + (new Random().nextInt(5) + 1) + ".jpg").getImage().getScaledInstance(screenSize.width * 5 / 6, screenSize.height * 9 / 10, Image.SCALE_SMOOTH)));
         generalLabelChooseName.setPreferredSize(new Dimension(screenSize.width * 5 / 6, screenSize.height * 8 / 10 + 65));
         generalLabelChooseName.setLayout(new GridBagLayout());
 
