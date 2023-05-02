899514f9050f7a48aaeea1ff56894d1635ccd7e9
Davide-Ettori <ettodavi01@gmail.com>
2023-05-02 15:51:19 +0200

    small fix

--- src/main/java/app/controller/Game.java
+++ src/main/java/app/controller/Game.java
@@ -32,7 +32,7 @@ import static javax.swing.JOptionPane.showMessageDialog;
  */
 public class Game extends UnicastRemoteObject implements Serializable, GameI {
     /** variable that represent the standard timer of the app for advancing the state of the game */
-    public static final double waitTimer = 1.5;
+    public static final double waitTimer = 2;
     /** variable that represent the standard timer of the app for showing events */
     public static final double showTimer = 2;
     /** variable that represent if we want to run or debug our application */
