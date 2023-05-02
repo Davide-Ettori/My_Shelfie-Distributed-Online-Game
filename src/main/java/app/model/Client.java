d0c400f82f45c05c74bbcd296df1a583137097b7
Samuele Giammusso <samuele.giammusso@mail.polimi.it>
2023-05-02 13:02:55 +0200

    prototype of GUI for Client and Server setup

--- src/main/java/app/model/Client.java
+++ src/main/java/app/model/Client.java
@@ -7,12 +7,15 @@ import app.view.IP;
 import app.view.TUI.PlayerTUI;
 import app.view.UIMode;
 
+import javax.swing.*;
+import java.awt.*;
 import java.io.*;
 import java.net.Socket;
 import java.rmi.RemoteException;
 
 import static app.model.NetMode.RMI;
 import static app.model.NetMode.SOCKET;
+import static app.view.Dimensions.textCols;
 import static app.view.UIMode.GUI;
 import static app.view.UIMode.TUI;
 
@@ -24,26 +27,58 @@ public class Client {
 
     /** variable used to keep track of the UI mode that the player is currently using */
     public static UIMode uiModeCur;
+    /** variable used to give a weight to the threads*/
     public static int threadWeight = 8;
+    private final transient Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+    private void tryToChooseIP(){
+
+    }
+
     /**
      * main method which is used by the user to choose the UI and the Network
      @author Ettori
      */
     public Client() {
+        //Panel and Frame
+        JFrame setupFrame = new JFrame("Setup of the game");
+        JPanel setupPanel = new JPanel(new GridBagLayout());
+        //Label for the Wallpaper
+        JLabel generalLabelChooseName = new JLabel(new ImageIcon(new ImageIcon("assets/Publisher material/Display_5.jpg").getImage().getScaledInstance(screenSize.width * 5 / 6, screenSize.height * 9 / 10, Image.SCALE_SMOOTH)));
+        generalLabelChooseName.setPreferredSize(new Dimension(screenSize.width * 5 / 6, screenSize.height * 8 / 10 + 65));
+        generalLabelChooseName.setLayout(new GridBagLayout());
+        //Informative text
+        JTextField informativeText = new JTextField(120);
+        informativeText.setText(" Here you can see the last relevant information about the setup ");
+        informativeText.setMinimumSize(new Dimension(textCols * 45, textCols + 8));
+        informativeText.setEditable(false);
+        informativeText.setBorder(null);
+        //IP Button and text field
+        JButton sendIP = new JButton("Choose IP Address");
+        JTextField textIP = new JTextField(20);
+        textIP.setBounds(100, 20, 165, 25);
+        textIP.addActionListener(event -> sendIP.doClick());
+
         String numPlayers;
         int numP;
         boolean flag = false;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("\nInsert the IP address of the server: ");
         String ip;
-        try {
-            ip = br.readLine();
-        } catch (IOException exc) {
-            System.out.println("error");
-            throw new RuntimeException(exc);
-        }
-        if(!ip.equals(""))
-            IP.activeIP = ip;
+        String tempIP;
+
+        sendIP.addActionListener((event) ->{
+            while(true){
+                try {
+                    ip = br.readLine();
+                } catch (IOException exc) {
+                    System.out.println("error");
+                    throw new RuntimeException(exc);
+                }
+                if(!ip.equals(""))
+                    IP.activeIP = ip;
+            }
+        });
 
         try {
             Socket mySocket = new Socket(IP.activeIP, Initializer.PORT);
