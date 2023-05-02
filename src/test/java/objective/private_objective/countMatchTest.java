ebb6adaab16e09da83029991ca3aa9f3a1fe13ab
Gioforno <samuelefac@gmail.com>
2023-05-02 22:20:40 +0200

    fix

--- src/test/java/objective/private_objective/countMatchTest.java
+++ src/test/java/objective/private_objective/countMatchTest.java
@@ -8,7 +8,6 @@ import org.junit.Test;
 
 import static app.model.Color.*;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 /**
  * Test the different number of goals and the different private objective
  * @author Furkan
@@ -259,7 +258,6 @@ public class countMatchTest {
 
         assertEquals(new PrivateObjective(objMat, 1).countPoints(lib), 9);
     }
-
     @Test
     public void countMatch_personalGoal_6match(){
         Card[][] objMat = new Card[6][5];
