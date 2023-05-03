3c7d0a778349e9e82e7924e8553a1be6ca89155f
Davide-Ettori <ettodavi01@gmail.com>
2023-05-03 21:21:50 +0200

    small fix

--- src/main/java/app/view/TUI/PlayerTUI.java
+++ src/main/java/app/view/TUI/PlayerTUI.java
@@ -586,6 +586,9 @@ public class PlayerTUI extends Player implements Serializable, PlayerI{
         gameStatus.put("points", pointsUntilNow);
         gameStatus.put("player", new PlayerSend(this));
         sendToServer(new Message(UPDATE_GAME, name, gameStatus));
+        if(netMode == SOCKET)
+            Game.waitForSeconds(Game.waitTimer);
+        waitForEvents();
     }
     /**
      * stops all the thread interaction related to the chat (should be only ReceiveChat)
