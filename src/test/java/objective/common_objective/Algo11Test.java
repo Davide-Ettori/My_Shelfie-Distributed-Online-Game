779c4f7bbd4acc15e8fa0f653d41a157c584fe6e
gfurkan00 <furkan.gumus@mail.polimi.it>
2023-05-07 09:25:57 +0200

    small fix

--- src/test/java/objective/common_objective/Algo11Test.java
+++ src/test/java/objective/common_objective/Algo11Test.java
@@ -32,6 +32,7 @@ public class Algo11Test {
      * <p></p>
      * <p> testing library:
      * <table border="1">
+     *     <caption>matrix</caption>
      * <tr><td>P</td><td>P</td><td>G</td><td>G</td><td>Y</td></tr>
      * <tr><td>B</td><td>B</td><td>&nbsp;&nbsp;&nbsp;</td><td>Y</td><td>&nbsp;&nbsp;&nbsp;</td></tr>
      * <tr><td>C</td><td>C</td><td>Y</td><td>B</td><td>B</td></tr>
@@ -87,6 +88,7 @@ public class Algo11Test {
      * <p></p>
      * <p> testing library:
      * <table border="1">
+     *     <caption>matrix</caption>
      * <tr><td>P</td><td>P</td><td>G</td><td>G</td><td>Y</td></tr>
      * <tr><td>B</td><td>B</td><td>&nbsp;&nbsp;&nbsp;</td><td>Y</td><td>&nbsp;&nbsp;&nbsp;</td></tr>
      * <tr><td>C</td><td>C</td><td>Y</td><td>B</td><td>B</td></tr>
