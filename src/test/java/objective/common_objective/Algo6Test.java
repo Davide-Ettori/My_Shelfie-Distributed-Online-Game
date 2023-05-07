97716d9a3d6c35608c4f6934d534ca90f5df3dcb
gfurkan00 <furkan.gumus@mail.polimi.it>
2023-05-07 09:23:12 +0200

    small fix

--- src/test/java/objective/common_objective/Algo6Test.java
+++ src/test/java/objective/common_objective/Algo6Test.java
@@ -36,6 +36,7 @@ public class Algo6Test {
      * <p></p>
      * <p> testing library:
      * <table border="1">
+     *     <caption>matrix</caption>
      * <tr><td>P</td><td>P</td><td>G</td><td>G</td><td>Y</td></tr>
      * <tr><td>B</td><td>B</td><td>&nbsp;&nbsp;&nbsp;</td><td>Y</td><td>P</td></tr>
      * <tr><td>C</td><td>C</td><td>Y</td><td>B</td><td>B</td></tr>
@@ -91,6 +92,7 @@ public class Algo6Test {
      * <p></p>
      * <p> testing library:
      * <table border="1">
+     *     <caption>matrix</caption>
      * <tr><td>P</td><td>P</td><td>G</td><td>G</td><td>Y</td></tr>
      * <tr><td>B</td><td>B</td><td>&nbsp;&nbsp;&nbsp;</td><td>Y</td><td>P</td></tr>
      * <tr><td>C</td><td>C</td><td>Y</td><td>B</td><td>B</td></tr>
@@ -146,6 +148,7 @@ public class Algo6Test {
      * <p></p>
      * <p> testing library:
      * <table border="1">
+     *     <caption>matrix</caption>
      * <tr><td>P</td><td>P</td><td>G</td><td>G</td><td>Y</td></tr>
      * <tr><td>P</td><td>B</td><td>&nbsp;&nbsp;&nbsp;</td><td>Y</td><td>P</td></tr>
      * <tr><td>C</td><td>C</td><td>Y</td><td>B</td><td>B</td></tr>
