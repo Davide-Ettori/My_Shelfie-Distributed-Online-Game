cbba91e5c0af18bd159aff88dae6121b7fa486cb
Davide-Ettori <ettodavi01@gmail.com>
2023-05-13 11:37:40 +0200

    small fix

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -4,7 +4,7 @@ import it.polimi.ingsw.model.Client;
 
 
 /**
- * first class called in the game, it starts the main process which will lead to the start of the complete game (My Shelfie)
+ * first class called in the game, it starts the main process which will lead to the start of the complete game
  * @author Ettori
  */
 public class App { public static void main(String[] args){new Client();}}
