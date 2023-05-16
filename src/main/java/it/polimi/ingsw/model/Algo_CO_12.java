03e8b71c9f23be89bae3fd9c70b90d74e7330bb1
Davide-Ettori <ettodavi01@gmail.com>
2023-05-16 15:45:45 +0200

    fixed algo 12in the model

--- src/main/java/it/polimi/ingsw/model/Algo_CO_12.java
+++ src/main/java/it/polimi/ingsw/model/Algo_CO_12.java
@@ -9,64 +9,13 @@ Five columns of increasing or decreasing height. Starting from the first column 
  */
 public class Algo_CO_12 extends Strategy {
     /**
-     * check for triangle, with diagonal
-     * @author Ettori
-     * @param board matrix
-     * @return true iff it find a triangle
-     */
-    private boolean checkLowTriangle_1(Card[][] board) {
-        for (int i = 0; i < ROWS; i++) {
-            for (int j = 0; j < COLS; j++) {
-                if (i >= j && board[i][j].color == Color.EMPTY)
-                    return false;
-                if (i < j && board[i][j].color != Color.EMPTY)
-                    return false;
-            }
-        }
-        return true;
-    }
-    /**
-     * check for triangle, no diagonal
-     * @author Ettori
-     * @param board matrix
-     * @return true iff it find a triangle
-     */
-    private boolean checkLowTriangle_2(Card[][] board) {
-        for (int i = 0; i < ROWS; i++) {
-            for (int j = 0; j < COLS; j++) {
-                if (i > j && board[i][j].color == Color.EMPTY)
-                    return false;
-                if (i <= j && board[i][j].color != Color.EMPTY)
-                    return false;
-            }
-        }
-        return true;
-    }
-    /**
-     * invert all the rows of a matrix and return a new one
-     * @author Ettori
-     * @param board matrix
-     * @return matrix with all the rows inverted
-     */
-    private Card[][]invert(Card[][] board){
-        Card[][] res = new Card[ROWS][COLS];
-        int k;
-        for(int i = 0; i < ROWS; i++){
-            k = 0;
-            for(int j = COLS - 1; j >= 0; j--){
-                res[i][k++] = board[i][j];
-            }
-        }
-        return res;
-    }
-    /**
      * checks if the array is sorted, strictly
-     * @param arr the interger array to check
+     * @param arr the integer array to check
      * @return true iff the array is sorted with no duplicates
      */
     private boolean checkSort(int[] arr){
         for(int i = 1; i < COLS; i++){
-            if(arr[i - 1] >= arr[i])
+            if(arr[i - 1] + 1 != arr[i])
                 return false;
         }
         return true;
